4a7830bb72751f5ea198543e26bdedbe5bc39a60
simsim <simeon.campbell@live.com>
2024-02-18 14:26:35 +0700

    init commit

---
+++ contracts/OrderSystem.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract OrderSystem {
+    enum OrderState {
+        Created,
+        Confirmed,
+        Delivered
+    }
+
+    struct UserProfile {
+        string name;
+        string age;
+        uint[] currentOrders;
+        uint[] completedOrders;
+    }
+
+    struct Order {
+        uint id;
+        address customer;
+        uint amount;
+        OrderState state;
+    }
+
+    mapping(address => UserProfile) private profiles;
+
+    mapping(uint => Order) private orders;
+
+    uint private orderId = 0;
+
+    function createOrder(
+        address _customer,
+        uint _amount
+    ) public returns (uint) {
+        uint currentOrderId = orderId;
+        orderId++;
+        UserProfile storage profile = profiles[_customer];
+        profile.currentOrders.push(currentOrderId);
+        orders[currentOrderId] = Order(
+            currentOrderId,
+            _customer,
+            _amount,
+            OrderState.Created
+        );
+        return currentOrderId;
+    }
+
+    function confirmOrder(uint OrderToBeConfirmed) public {
+        Order storage order = orders[OrderToBeConfirmed];
+        require(order.state == OrderState.Created, "Order already confirmed");
+        order.state = OrderState.Confirmed;
+    }
+
+    function deliverOrder(uint deliveredOrder) public {
+        Order storage order = orders[deliveredOrder];
+        UserProfile storage profile = profiles[order.customer];
+        require(order.state == OrderState.Confirmed, "Order not confirmed");
+        require(
+            msg.sender == order.customer,
+            "Only customer can confirm delivery"
+        );
+        order.state = OrderState.Delivered;
+
+        for (uint i = 0; i < profile.currentOrders.length; i++) {
+            if (profile.currentOrders[i] == deliveredOrder) {
+                profile.currentOrders[i] = profile.currentOrders[
+                    profile.currentOrders.length - 1
+                ];
+                profile.currentOrders.pop();
+                break;
+            }
+        }
+
+        profile.completedOrders.push(deliveredOrder);
+    }
+
+    function setProfile(string memory _name, string memory _age) public {
+        profiles[msg.sender].name = _name;
+        profiles[msg.sender].age = _age;
+    }
+
+    function getProfile(
+        address _user
+    ) public view returns (string memory, string memory) {
+        return (profiles[_user].name, profiles[_user].age);
+    }
+
+    function getOrderState(uint orderId) public view returns (OrderState) {
+        return orders[orderId].state;
+    }
+
+    function getOrder(uint orderId) public view returns (Order memory) {
+        return orders[orderId];
+    }
+
+    function getOrders(address _user) public view returns (uint[] memory) {
+        return profiles[_user].currentOrders;
+    }
+}
---
+++ test/OrderSystem.test.js
@@ -0,0 +1,56 @@
+const { expect } = require("chai")
+const { ethers } = require("hardhat")
+
+describe("OrderSystem", function () {
+    let OrderSystem, orderSystem, owner, addr1
+    const orderId = 0
+    const orderAmount = 100
+
+    beforeEach(async function () {
+        OrderSystem = await ethers.getContractFactory("OrderSystem")
+        ;[owner, addr1] = await ethers.getSigners()
+        orderSystem = await OrderSystem.deploy()
+    })
+
+    describe("createOrder", function () {
+        it("Should create an order", async function () {
+            await orderSystem.createOrder(owner.address, orderAmount)
+            const order = await orderSystem.getOrder(orderId)
+            expect(order.id).to.equal(orderId)
+            expect(order.customer).to.equal(owner.address)
+            expect(order.amount).to.equal(orderAmount)
+            expect(order.state).to.equal(0) // OrderState.Created
+            console.log(order.id.toString())
+        })
+    })
+    describe("confirmOrder", function () {
+        it("Should confirm an order", async function () {
+            await orderSystem.createOrder(owner.address, orderAmount)
+            await orderSystem.confirmOrder(orderId)
+            const order = await orderSystem.getOrder(orderId)
+            expect(order.state).to.equal(1) // OrderState.Confirmed
+        })
+    })
+
+    describe("deliverOrder", function () {
+        it("Should deliver an order", async function () {
+            await orderSystem.createOrder(owner.address, orderAmount)
+            await orderSystem.confirmOrder(orderId)
+            await orderSystem.deliverOrder(orderId)
+            const order = await orderSystem.getOrder(orderId)
+            expect(order.state).to.equal(2) // OrderState.Delivered
+        })
+    })
+    describe("setProfile", function () {
+        it("Should set and get a user profile", async function () {
+            const name = "Alice"
+            const age = "25"
+            await orderSystem.connect(owner).setProfile(name, age)
+            const [profileName, profileAge] = await orderSystem.getProfile(
+                owner.address,
+            )
+            expect(profileName).to.equal(name)
+            expect(profileAge).to.equal(age)
+        })
+    })
+})
